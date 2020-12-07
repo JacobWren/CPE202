2968ca429e5a62852e463e00d24b3cb17c28a7b4
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-07 14:15:04 -0800

    Update stack_tests.py

--- Labs/lab2/stack_tests.py
+++ Labs/lab2/stack_tests.py
@@ -2,10 +2,10 @@ import unittest
 
 # Use the imports below to test either your array-based stack
 # or your link-based version
-#from stack_array import Stack
 from stack_linked import *
 
 class TestLab2(unittest.TestCase):
+    
     def test_simple(self):
         stack = Stack(5)
         stack.push(0)
@@ -13,6 +13,7 @@ class TestLab2(unittest.TestCase):
         self.assertFalse(stack.is_full())
         self.assertEqual(stack.size(),1)
 
+        
     def test_simple2(self):
         stack = Stack(5)
         stack.push(0)
@@ -22,6 +23,7 @@ class TestLab2(unittest.TestCase):
         stack.pop()
         self.assertTrue(stack.is_empty())
 
+        
     def test_simple3(self):
         stack = Stack(5)
         stack.push(0)
@@ -29,6 +31,7 @@ class TestLab2(unittest.TestCase):
         with self.assertRaises(IndexError):
             stack.pop()
 
+            
     def test_simple4(self):
         stack = Stack(3)
         stack.push(0)
@@ -37,6 +40,7 @@ class TestLab2(unittest.TestCase):
         with self.assertRaises(IndexError):
             stack.push(0)
 
+            
     def test_simple5(self):
         stack = Stack(5)
         stack.push(0)
@@ -44,6 +48,7 @@ class TestLab2(unittest.TestCase):
         with self.assertRaises(IndexError):
             stack.peek()
 
+            
     def test_simple6(self):
         stack = Stack(5)
         stack.push(0)
@@ -54,6 +59,7 @@ class TestLab2(unittest.TestCase):
         stack.pop()
         self.assertTrue(stack.is_empty())
 
+        
     def test_simple7(self):
         stack = Stack(4)
         stack.push(1)
@@ -63,6 +69,7 @@ class TestLab2(unittest.TestCase):
         self.assertEqual(stack.peek(), 4)
         self.assertTrue(stack.is_full())
 
+        
     def test_simple8(self):
         stack = Stack(3)
         stack.push(0)
@@ -74,6 +81,7 @@ class TestLab2(unittest.TestCase):
         stack.push(5)
         self.assertEqual(stack.pop(), 5)
 
+        
     def test_simple9(self):
         stack = Stack(3)
         stack.push(0)
