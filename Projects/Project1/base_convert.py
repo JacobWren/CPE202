e2ed830735d1554573c33ebe38ed00a94cda1626
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-07 12:33:02 -0800

    Update base_convert.py

--- Projects/Project1/base_convert.py
+++ Projects/Project1/base_convert.py
@@ -1,60 +1,12 @@
-def convert(num, b):
-    """Recursive function that returns a string representing num in the base b"""
-    # base case!
-    if num // b == 0:
-        return str(num)
+def convert(x, y):
+    x = convert_helper(x, y)
+    if len(x) > 1 and x[0] == '0':
+        return x[1:]
     else:
-        # base symbols
-        if num - ((num // b ) * b) == 10:
-            return convert(num // b, b) + 'A'
-        elif num - ((num // b ) * b) == 11:
-            return convert(num // b, b) + 'B'
-        elif num - ((num // b ) * b) == 12:
-            return convert(num // b, b) + 'C'
-        elif num - ((num // b ) * b) == 13:
-            return convert(num // b, b) + 'D'
-        elif num - ((num // b ) * b) == 14:
-            return convert(num // b, b) + 'E'
-        elif num - ((num // b ) * b) == 15:
-            return convert(num // b, b) + 'F'
-        # need 'str(num - ((num // b) * b))' to 'get concatenation going'
-    return convert(num // b, b) + str(num - ((num // b) * b))
-#print(convert(389, 15))
-#self.assertEqual(convert(11259375,16),"ABCDEF")
+        return x
 
-
-def convert(num, b):
-    """Recursive function that returns a string representing num in the base b"""
-    # base symbols
-    if num - ((num // b ) * b) == 10:
-        return convert(num // b, b) + 'A'
-    elif num - ((num // b ) * b) == 11:
-        return convert(num // b, b) + 'B'
-    elif num - ((num // b ) * b) == 12:
-        return convert(num // b, b) + 'C'
-    elif num - ((num // b ) * b) == 13:
-        return convert(num // b, b) + 'D'
-    elif num - ((num // b ) * b) == 14:
-        return convert(num // b, b) + 'E'
-    elif num - ((num // b ) * b) == 15:
-        return convert(num // b, b) + 'F'
-    elif num // b == 0 and num != 0:
-        return str(num)
-
-    elif num == 0:
-        return ""
-        # need ' + str(num - ((num // b) * b))' to 'get concatenation going'
-    return convert(num // b, b) + str(num - ((num // b) * b))
-#print(convert(11259375,16))
-#print(convert(30,4))
-#self.assertEqual(convert(11259375,16),"ABCDEF")
-
-#self.assertEqual(convert(i, 10),str(i))
-#print(convert(0, 10))
-
-
-
-def convert_1(num, b):
+    
+def convert_helper(num, b):
     """Recursive function that returns a string representing num in the base b"""
     # base symbols
     if num - ((num // b ) * b) == 10:
@@ -77,23 +29,6 @@ def convert_1(num, b):
     # value or we had > 1 recursive calls to get here. It turns out making that distinction requires us to use a flag.
     # Thus we will wrongly always add a zero to the start of the string if num == 0. We fix this in the function below.
     elif num == 0:
-        #return ""
         return str(num)
-        # need ' + str(num - ((num // b) * b))' to 'get concatenation going'
+        # need ' + str(num - ((num // b) * b))' to "get concatenation going"
     return convert_1(num // b, b) + str(num - ((num // b) * b))
-#print(convert(11259375,16))
-#print(convert(30,4))
-#self.assertEqual(convert(11259375,16),"ABCDEF")
-
-#self.assertEqual(convert(i, 10),str(i))
-#print(convert(0, 10))
-
-
-def convert(x, y):
-    x = convert_1(x, y)
-    if len(x) > 1 and x[0] == '0':
-        return x[1:]
-    else:
-        return x
-
-print( convert(1029, 2) )
