427b3ad1b24d32fc59a099c19d73b071aa229f89
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-07 13:58:43 -0800

    Delete planets.py

--- Labs/planets.py
+++
@@ -1,8 +0,0 @@
-def weight_on_planets():
-    
-    weight = int(input("What do you weigh on earth? "))
-    print("\n" + "On Mars you would weigh {0} pounds.".format(weight * .38) +
-          "\n" + "On Jupiter you would weigh {0} pounds.".format(weight * 2.34))
-    
-if __name__ == '__main__':
-    weight_on_planets()
