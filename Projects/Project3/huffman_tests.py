372814f1836477b2724ed90f3591f8b3c584378b
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-07 13:30:16 -0800

    Update huffman_tests.py

--- Projects/Project3/huffman_tests.py
+++ Projects/Project3/huffman_tests.py
@@ -36,43 +36,13 @@ class TestList(unittest.TestCase):
         self.assertEqual(codes[ord('d')], '1')
         self.assertEqual(codes[ord('a')], '0000')
         self.assertEqual(codes[ord('f')], '0001')
-    '''
-    def test_01_textfile(self):
-        huffman_encode("file1.txt", "file1_out.txt")
-        # capture errors by running 'diff' on your encoded file with a *known* solution file
-        err = subprocess.call("diff -wb file1_out.txt file1_soln.txt", shell = True)
-        self.assertEqual(err, 0)
-    
-    def test_02_textfile(self):
-        huffman_encode("file2.txt", "file2_out.txt")
-        # capture errors by running 'diff' on your encoded file with a *known* solution file
-        err = subprocess.call("diff -wb file2_out.txt file2_soln.txt", shell = True)
-        self.assertEqual(err, 0)
-    '''
+
     def test_03_textfile(self):
         huffman_encode("declaration.txt", "declaration_out.txt")
         # capture errors by running 'diff' on your encoded file with a *known* solution file
         err = subprocess.call("diff -wb declaration_out.txt declaration_soln.txt", shell = True)
         self.assertEqual(err, 0)
-    '''
-    def test_04_textfile(self):
-        huffman_encode("multiline.txt", "multiline_out.txt")
-        # capture errors by running 'diff' on your encoded file with a *known* solution file
-        err = subprocess.call("diff -wb multiline_out.txt multiline_soln.txt", shell = True)
-        self.assertEqual(err, 0)
-    
-    def test_05_textfile(self):
-        huffman_encode("file3.txt", "file3_out.txt")
-        # capture errors by running 'diff' on your encoded file with a *known* solution file
-        err = subprocess.call("diff -wb file3_out.txt file3_soln.txt", shell = True)
-        self.assertEqual(err, 0)
-    
-    def test_06_textfile(self):
-        huffman_decode("file1_out.txt", "file1_decode.txt")
-        # capture errors by running 'diff' on your encoded file with a *known* solution file
-        err = subprocess.call("diff -wb file1_decode.txt file1.txt", shell = True)
-        self.assertEqual(err, 0)
-    '''
+
     def test_07_textfile(self):
         with self.assertRaises(FileNotFoundError):
             huffman_decode("Jimmy", "James")
