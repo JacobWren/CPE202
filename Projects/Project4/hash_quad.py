8789d8ad03d5c41f88666046b7766cda94a90727
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-07 13:47:57 -0800

    Update hash_quad.py

--- Projects/Project4/hash_quad.py
+++ Projects/Project4/hash_quad.py
@@ -1,29 +1,24 @@
 class HashTable:
 
-    def __init__(self, table_size = 191):         # can add additional attributes
-        self.table_size = table_size        # initial table size
-        #self.hash_table = None*table_size # hash table
-        self.num_items = 0                  # empty hash table
-        #self.store_value = [None]*table_size
+    def __init__(self, table_size = 191): # can add additional attributes
+        self.table_size = table_size # initial table size
+        self.num_items = 0 # empty hash table
         self.load_factor = 0
         self.hash_table = [None] * table_size
 
+        
     def insert(self, key, value):
         """ Inserts an entry into the hash table (using Horner hash function to determine index, 
         and quadratic probing to resolve collisions).
+        
         The key is a string (a word) to be entered, and value is the line number that the word appears on. 
         If the key is not already in the table, then the key is inserted, and the value is used as the first 
         line number in the list of line numbers. If the key is in the table, then the value is appended to that 
         key’s list of line numbers. If value is not used for a particular hash table (e.g. the stop words hash table),
         can use the default of 0 for value and just call the insert function with the key.
         If load factor is greater than 0.5 after an insertion, hash table size should be increased (doubled + 1)."""
+        
         hash_value = self.horner_hash(key)
-        #if type(value) == list and len(value) == 1:
-        #if type(value) == list:
-            #if l.shape[0] == 1:
-                #value = str(value)
-                #value = value.replace("]", "").replace("[", "")
-                #value = int(value)
         if self.hash_table[hash_value] == None: # Then insert it!
             self.hash_table[hash_value] = (key,[])
             self.hash_table[hash_value][1].append(value)
@@ -32,14 +27,9 @@ class HashTable:
             if self.load_factor > .5:
                 self.num_items = 0
                 temp_hash_table = []
-                #for j in self.hash_table:
-                    #if j != None:
-                        #temp_hash_table.append(j)
                 temp_hash_table = list(filter(None, self.hash_table))
                 self.table_size = (self.table_size * 2) + 1
                 self.hash_table = [None] * self.table_size
-                #for i in range(len(temp_hash_table)):
-                    #self.insert(temp_hash_table[i][0], temp_hash_table[i][1])
                 for i in range(len(temp_hash_table)):
                     for j in temp_hash_table[i][1]:
                         self.insert(temp_hash_table[i][0], j)
@@ -72,19 +62,16 @@ class HashTable:
                         for i in range(len(temp_hash_table)):
                             for j in temp_hash_table[i][1]:
                                 self.insert(temp_hash_table[i][0], j)
-                                #self.insert(temp_hash_table[i][0], temp_hash_table[i][1])
                 elif self.hash_table[quad_slot][0] == key and value in self.hash_table[quad_slot][1]:
                     return
                 else:
                     self.hash_table[quad_slot][1].append(value)
 
 
-
-
-
     def horner_hash(self, key):
         """ Compute and return an integer from 0 to the (size of the hash table) - 1
         Compute the hash value by using Horner’s rule, as described in project specification."""
+        
         char_list = list(key)
         n = min(8, len(char_list))
         cum_sum = 0
@@ -95,10 +82,12 @@ class HashTable:
 
     def rehash(self, old_hash, i):
         """ Compute and return new hash value"""
+        
         return (old_hash + i**2) % self.table_size
 
     def in_table(self, key):
         """ Returns True if key is in an entry of the hash table, False otherwise."""
+        
         if all(i is None for i in self.hash_table):
             return False
         hash_value = self.horner_hash(key)
@@ -108,13 +97,10 @@ class HashTable:
             return True
         else:
             i = 0
-            #hash_value = self.rehash(hash_value, i)
             og_hash = hash_value
             while self.hash_table[hash_value] != None and self.hash_table[hash_value][0] != key:
                 i += 1
                 hash_value = self.rehash(og_hash, i)
-            #if self.hash_table[hash_value] == None:
-                #return False
             else:
                 if self.hash_table[hash_value] == None:
                     return False
@@ -125,6 +111,7 @@ class HashTable:
     def get_index(self, key):
         """ Returns the index of the hash table entry containing the provided key. 
         If there is not an entry with the provided key, returns None."""
+        
         hash_value = self.horner_hash(key)
         if self.hash_table[hash_value] == None:
             return None
@@ -132,7 +119,6 @@ class HashTable:
             return hash_value
         else:
             i = 0
-            #hash_value = self.rehash(hash_value, i)
             og_hash = hash_value
             while self.hash_table[hash_value] != None:
                 if self.hash_table[hash_value][0] == key:
@@ -142,16 +128,17 @@ class HashTable:
                     hash_value = self.rehash(og_hash, i)
             if self.hash_table[hash_value] == None:
                 return None
-            #else:
-                #return hash_value
 
+            
     def get_all_keys(self):
         """ Returns a Python list of all keys in the hash table."""
         return [i[0] for i in self.hash_table if i]
 
+    
     def get_value(self, key):
         """ Returns the value (list of line numbers) associated with the key. 
         If key is not in hash table, returns None."""
+        
         if all(i is None for i in self.hash_table):
             return None
         hash_value = self.horner_hash(key)
@@ -161,7 +148,6 @@ class HashTable:
             return self.hash_table[hash_value][1]
         else:
             i = 0
-            #hash_value = self.rehash(hash_value, i)
             og_hash = hash_value
             while self.hash_table[hash_value] != None:
                 if self.hash_table[hash_value][0] == key:
@@ -171,20 +157,21 @@ class HashTable:
                     hash_value = self.rehash(og_hash, i)
             if self.hash_table[hash_value] == None:
                 return None
-            #else:
-                #return self.hash_table[hash_value][1]
 
 
     def get_num_items(self):
         """ Returns the number of entries (words) in the table."""
+        
         return self.num_items
 
     def get_table_size(self):
         """ Returns the size of the hash table."""
+        
         return self.table_size
 
     def get_load_factor(self):
         """ Returns the load factor of the hash table (entries / table_size)."""
+        
         return (self.num_items / self.table_size)
 
 '''
@@ -196,5 +183,4 @@ ht.insert("cbQ", 6)
 ht.insert("cap", 7)
 ht.insert("m", 6)
 ht.get_value("capo")
-
 '''
